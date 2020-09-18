6e2c4896a38ab3521a1621e1d350dee895516cb2
Felippe Augusto Alacoque Rodrigues Mariano <felippealacoque@gmail.com>
2020-09-17 22:26:49 -0300

    Correção repositórios de testes

--- src/test/java/com/crud/CrudApplicationTests.java
+++ src/test/java/com/crud/CrudApplicationTests.java
@@ -3,16 +3,14 @@ package com.crud;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("test")
 public class CrudApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
