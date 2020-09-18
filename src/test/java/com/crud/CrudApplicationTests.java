5d9d2f006dfa0f6c5de4f0b63965d030c73b1d68
Felippe Augusto Alacoque Rodrigues Mariano <felippealacoque@gmail.com>
2020-09-17 22:21:03 -0300

    Correções classes de testes

--- src/test/java/com/crud/CrudApplicationTests.java
+++ src/test/java/com/crud/CrudApplicationTests.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
-class CrudApplicationTests {
+public class CrudApplicationTests {
 
 	@Test
 	void contextLoads() {
--- src/test/java/com/crud/repository/LancamentoRepositoryTest.java
+++ src/test/java/com/crud/repository/LancamentoRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.crud.repository;
 
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -10,16 +9,5 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 @ActiveProfiles("test")
 public class LancamentoRepositoryTest {
-	
-	@Autowired
-	EmpresaRepository empresaRepository;
-	
-	@Autowired
-	FuncionarioRepository funcionarioRepository;
-	
-	@Autowired
-	LancamentoRepository lancamentoRepository;
-	
-	private Long funcionarioId;
 
 }
