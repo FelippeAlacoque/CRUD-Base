01f446e8eb21c739aaacb583bbe58142153a4b22
Felippe Augusto Alacoque Rodrigues Mariano <felippealacoque@gmail.com>
2020-09-17 22:38:52 -0300

    Anotações JUnit

---
+++ src/test/java/com/crud/repository/EmpresaRepositoryTest.java
@@ -0,0 +1,51 @@
+package com.crud.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.crud.model.Empresa;
+import com.google.common.util.concurrent.ExecutionError;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class EmpresaRepositoryTest {
+	
+	public EmpresaRepositoryTest() {
+		super();
+	}
+
+	@Autowired
+	EmpresaRepository empresaRepository;
+	
+	private static final String CNPJ = "83894778000119";
+	
+	@Before
+	public void setUpData() throws ExecutionError {
+		Empresa empresa = new Empresa();
+		empresa.setRazaoSocial("Empresa Teste");
+		empresa.setCnpj(CNPJ);
+		this.empresaRepository.save(empresa);
+	}
+	
+	@After
+	public final void destroy() {
+		this.empresaRepository.deleteAll();
+	}
+	
+	@Test
+	public void testBuscarPorCnpj() {
+		Empresa empresa = empresaRepository.findByCnpj(CNPJ);
+		
+		assertEquals(CNPJ, empresa.getCnpj());
+	}
+
+}
